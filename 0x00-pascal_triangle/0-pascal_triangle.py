398ef76043167db40ae9cb2128fe7695671e1ce4
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-13 12:00:08 +0300

    create: pascal solution file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,29 @@
-def pascal_triangle(n):
-    if n <= 0:
-        return []
+#!/usr/bin/env python3
 
-    triangle = [[1]]
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
-        triangle.append(new_row)
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+
+    Args:
+    - n: An integer representing the number of rows in Pascal's Triangle.
+
+    Returns:
+    - A list of lists containing the values of Pascal's Triangle.
+    """
+    def generate_row(row_num, prev_row):
+        if row_num == 0:
+            return []
+        elif row_num == 1:
+            return [1]
+        else:
+            new_row = [1]
+            for i in range(1, len(prev_row)):
+                new_row.append(prev_row[i-1] + prev_row[i])
+            new_row.append(1)
+            return new_row
+
+    triangle = []
+    for i in range(1, n + 1):
+        triangle.append(generate_row(i, triangle[-1] if triangle else []))
 
     return triangle
