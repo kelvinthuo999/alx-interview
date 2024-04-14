b60d2671318e54267b232c8103880632e6fda127
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-14 18:57:27 +0300

    create: pascal interview solution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,29 @@
+#!/usr/bin/python3
+'''Module to find Pascal's Triangle integers'''
+
 def pascal_triangle(n):
+    '''
+    Function to find Pascal's Triangle integers
+
+    Parameters:
+        n (int): The number of row's of Pascal's triangle
+
+    Returns:
+        pascal_triangle (list): Pascal's Triangle up to the nth row
+    '''
+    pascal_triangle = []
+
     if n <= 0:
-        return []
+        return pascal_triangle
 
-    triangle = [[1]]
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
-        triangle.append(new_row)
+    # Add rows to Pascal's Triangle
+    for i in range(n):
+        row = [None for _ in range(i+1)]
+        row[0], row[-1] = 1, 1  # Set first and last elements to 1
 
-    return triangle
+        for j in range(1, len(row)-1):
+            row[j] = pascal_triangle[i-1][j-1] + pascal_triangle[i-1][j]
+
+        pascal_triangle.append(row)
+
+    return pascal_triangle
