192961fe3bffa51fed3f5789b28d85a6257e2cd7
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-02 18:11:15 +0000

    create: script to parse log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,62 @@
 #!/usr/bin/python3
+'''function to parse log'''
+
 import sys
 
-def print_stats(total_size, status_codes):
-    print("File size: {}".format(total_size))
-    for code, count in sorted(status_codes.items()):
-        print("{}: {}".format(code, count))
 
-def parse_line(line):
-    parts = line.split()
-    if len(parts) < 9:
-        return None, None
-    ip_address = parts[0]
-    status_code = parts[-2]
-    file_size = parts[-1]
-    if not (status_code.isdigit() and file_size.isdigit()):
-        return None, None
-    return status_code, int(file_size)
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
-def main():
-    total_size = 0
-    status_codes = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            status_code, file_size = parse_line(line.strip())
-            if status_code is None:
-                continue
-            total_size += file_size
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            if i % 10 == 0:
-                print_stats(total_size, status_codes)
-    except KeyboardInterrupt:
-        pass
-    print_stats(total_size, status_codes)
+    print(f"File size: {total_file_size}")
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print(f"{key}: {val}")
+
+
+def process_lines(lines):
+    total_file_size = 0
+    code = 0
+    counter = 0
+    dict_sc = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+
+    for line in lines:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if code in dict_sc:
+                    dict_sc[code] += 1
+
+            if counter == 10:
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+    if counter != 0:
+        print_msg(dict_sc, total_file_size)
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        process_lines(sys.stdin)
+    except KeyboardInterrupt:
+        pass
