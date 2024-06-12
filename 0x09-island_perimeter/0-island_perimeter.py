f3587fc2f3df3b28de4f3e4834303625247d0680
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-06-12 13:13:44 +0000

    create: function to calc island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """
-Funct to calc perimeter
+Funct to calc perimeter of an island
 """
 
 
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island represented by the grid.
-    
+
     Args:
     grid (list[list[int]]): A list of lists representing the island grid.
-    
+
     Returns:
     int: The perimeter of the island.
     """
     perimeter = 0
-    
+
     # Iterate over each row in the grid
     for i in range(len(grid)):
         # Iterate over each column in the current row
         for j in range(len(grid[i])):
-            # Check if the current cell is land (1) and has a neighboring water cell (0)
             if grid[i][j] == 1:
                 # Check top cell
                 if i == 0 or grid[i - 1][j] == 0:
@@ -34,5 +33,5 @@ def island_perimeter(grid):
                 # Check right cell
                 if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-                
+
     return perimeter
