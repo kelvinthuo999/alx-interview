1d22201d092829adfba88e85e04f584015862618
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-06-19 09:56:10 +0000

    compliance with pycodestyle guidelines

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""0. Prime Game - Maria and Ben are playing a game"""
+"""
+A prime game between Maria and Ben
+"""
+
 
 def isWinner(x, nums):
-    """Determine the winner after x rounds of the game."""
+    """
+    Determine the winner after x rounds of the game.
+    """
     if not nums or x <= 0:
         return None
 
     max_num = max(nums)
-    
+
     # Sieve of Eratosthenes to find all primes up to max_num
     is_prime = [True] * (max_num + 1)
     is_prime[0] = is_prime[1] = False
-    
+
     for i in range(2, int(max_num**0.5) + 1):
         if is_prime[i]:
             for j in range(i * i, max_num + 1, i):
@@ -25,19 +30,18 @@ def isWinner(x, nums):
             count += 1
         prime_count[i] = count
 
-    maria_wins = 0
-    ben_wins = 0
+    maria_victor = 0
+    ben_victor = 0
 
     for n in nums:
         if prime_count[n] % 2 == 0:
-            ben_wins += 1
+            ben_victor += 1
         else:
-            maria_wins += 1
+            maria_victor += 1
 
-    if maria_wins > ben_wins:
+    if maria_victor > ben_victor:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif ben_victor > maria_victor:
         return "Ben"
     else:
         return None
-
