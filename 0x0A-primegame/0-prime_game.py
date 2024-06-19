7a4d469108e9d64615d326f6c1e71693af4d41a5
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-06-19 09:32:12 +0000

    create: solution to the prime game problem

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,38 +1,53 @@
 #!/usr/bin/python3
 
-def sieve_of_eratosthenes(n):
-    """Generate a list of booleans indicating prime status of numbers up to n."""
-    primes = [True] * (n + 1)
-    p = 2
-    while (p * p <= n):
-        if (primes[p] == True):
-            for i in range(p * p, n + 1, p):
-                primes[i] = False
-        p += 1
-    primes[0], primes[1] = False, False  # 0 and 1 are not primes
-    return primes
+def is_winner(player):
+    return "Maria" if player == 0 else "Ben"
+
+def is_prime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 def isWinner(x, nums):
-    """Determine the winner of the prime game."""
-    if x < 1 or not nums:
-        return None
+    """
+    Determine the winner of the prime game.
     
-    max_num = max(nums)
-    primes = sieve_of_eratosthenes(max_num)
+    :param x: Number of rounds
+    :param nums: List of numbers for each round
+    :return: Name of the player that won the most rounds
+    """
+    winners = []
+    for i in range(x):
+        # Copy the current round's numbers for each player's turn
+        maria_nums = nums[i].copy()
+        ben_nums = nums[i].copy()
+        
+        # Simulate Maria's turn
+        for num in sorted(maria_nums, reverse=True):
+            if is_prime(num):
+                maria_nums = [n for n in maria_nums if n % num!= 0]
+                break
+        
+        # Simulate Ben's turn
+        for num in sorted(ben_nums, reverse=True):
+            if is_prime(num):
+                ben_nums = [n for n in ben_nums if n % num!= 0]
+                break
+        
+        # Update the winner count
+        winners.append(is_winner(i % 2))
     
-    maria_wins = 0
-    ben_wins = 0
+    # Count the winners
+    counts = {player: winners.count(player) for player in ["Maria", "Ben"]}
     
-    for n in nums:
-        primes_count = sum(primes[0:n+1])
-        if primes_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-    
-    if maria_wins > ben_wins:
+    # Determine the overall winner
+    if counts["Maria"] > counts["Ben"]:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif counts["Ben"] > counts["Maria"]:
         return "Ben"
     else:
         return None
