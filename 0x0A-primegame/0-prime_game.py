7eb4f934c26af9d353615680ea8e72b81ff69a16
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-06-19 09:49:25 +0000

    create: function to solve prime number game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-
-def sieve_of_eratosthenes(n):
-    """Generate a list of booleans indicating prime status and cumulative prime count up to n."""
-    primes = [True] * (n + 1)
-    p = 2
-    while (p * p <= n):
-        if primes[p] == True:
-            for i in range(p * p, n + 1, p):
-                primes[i] = False
-        p += 1
-    primes[0], primes[1] = False, False  # 0 and 1 are not primes
-
-    # Create a cumulative count of primes up to each index
-    prime_count = [0] * (n + 1)
-    count = 0
-    for i in range(n + 1):
-        if primes[i]:
-            count += 1
-        prime_count[i] = count
-    
-    return primes, prime_count
+"""0. Prime Game - Maria and Ben are playing a game"""
 
 def isWinner(x, nums):
-    """Determine the winner of the prime game."""
-    if x < 1 or not nums:
+    """Determine the winner after x rounds of the game."""
+    if not nums or x <= 0:
         return None
 
     max_num = max(nums)
-    _, prime_count = sieve_of_eratosthenes(max_num)
     
+    # Sieve of Eratosthenes to find all primes up to max_num
+    is_prime = [True] * (max_num + 1)
+    is_prime[0] = is_prime[1] = False
+    
+    for i in range(2, int(max_num**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_num + 1, i):
+                is_prime[j] = False
+
+    # Create a cumulative count of primes up to each index
+    prime_count = [0] * (max_num + 1)
+    count = 0
+    for i in range(1, max_num + 1):
+        if is_prime[i]:
+            count += 1
+        prime_count[i] = count
+
     maria_wins = 0
     ben_wins = 0
 
